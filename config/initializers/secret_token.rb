73897d23eccc826c6ac26396a9639cdd840528f7
Phillip Lackey <philliplackey@aimac.corp.athlon.com>
2012-12-17 17:02:50 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MobileBracket::Application.config.secret_token = 'a469b620c6183d2ec86b7f285309452d7f923aaadd56f48d90a2fab3b566e69e6fe46fea704b0de0288a41c76221953338c243ed87ada4b8978c4fd612d181ec'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MobileBracket::Application.config.session_store :cookie_store, key: '_mobile_bracket_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MobileBracket::Application.config.session_store :active_record_store
